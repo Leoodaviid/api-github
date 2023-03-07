bad6115144d048504f34c23c89845052a563eeee
Leoodaviid <david_ax7@hotmail.com>
2023-03-07 03:24:45 -0300

    Criando tela de busca + styles

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,25 @@
-import { Container, Logo, Title } from './styles'
+import { Container, Logo, Title, Form, Input, Button } from './styles'
+import { useState } from 'react'
+import { MdSearch } from 'react-icons/md'
 import GitHubLogo from '../../assets/icons/github-logo.svg'
 
 const Main = () => {
+  const [login, setLogin] = useState<string>()
+
   return (
     <Container>
       <Logo src={GitHubLogo} alt='Logo Api GitHub' />
       <Title>Api GitHub</Title>
+      <Form>
+        <Input
+          placeholder='Usuário'
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <Button to={`/${login}/repositories`}>
+          <MdSearch size={42} />
+        </Button>
+      </Form>
     </Container>
   )
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const Container = styled.div`
@@ -15,4 +16,49 @@ export const Logo = styled.img`
 export const Title = styled.h1`
   font-size: ${({ theme }) => theme.fontSize['3xl']};
   color: ${({ theme }) => theme.colors.white};
+`
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 583px;
+`
+export const Input = styled.input`
+  background: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+  width: 100%;
+  height: 64px;
+  line-height: 64px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  border-radius: 0.25rem;
+  font-size: ${({ theme }) => theme.fontSize['2xl']};
+  margin-right: 24px;
+  padding: 0 1.5rem;
+  box-sizing: border-box;
+
+  &:focus {
+    outline: none;
+    border-color: ${({ theme }) => theme.colors.white};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray700};
+  }
+`
+export const Button = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.gray700};
+  width: 80px;
+  height: 64px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  border-radius: 0.25rem;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.white};
+    color: ${({ theme }) => theme.colors.gray800};
+  }
 `
