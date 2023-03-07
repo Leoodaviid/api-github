8a5c23639fecd60c724f3f9705a351e8d20a0506
Leoodaviid <david_ax7@hotmail.com>
2023-03-07 16:36:21 -0300

    componente repository + styles

---
+++ src/components/Repository/Repository.tsx
@@ -0,0 +1,15 @@
+import { CardRepository } from '../CardRepository/CardRepository'
+import { Container } from './styles'
+
+export const Repository = () => {
+  return (
+    <Container>
+      <CardRepository />
+      <CardRepository />
+      <CardRepository />
+      <CardRepository />
+      <CardRepository />
+      <CardRepository />
+    </Container>
+  )
+}
