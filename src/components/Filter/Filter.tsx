41612e120052928eb4dac2f247104c31011af343
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 03:40:53 -0300

    Componente filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,14 +1,10 @@
 import { Container, Selector, Cleaner } from './styles'
 
-export const Filter = () => {
-  const langs = [
-    { name: 'JavaScript', count: 5, color: '#FCC419' },
-    { name: 'Shell', count: 2, color: '#23DB42' },
-    { name: 'PHP', count: 2, color: '#4263Eb' },
-    { name: 'TypeScript', count: 2, color: '#4263Eb' },
-  ]
-
-  const selectors = langs.map(({ name, count, color }) => (
+interface FilterLanguageProps {
+  languages: { name: string; count: number; color?: string }[]
+}
+export const Filter = ({ languages }: FilterLanguageProps) => {
+  const selectors = languages.map(({ name, count, color }) => (
     <Selector key={name.toLocaleLowerCase()} color={color}>
       <span>{name}</span>
       <span>{count}</span>
