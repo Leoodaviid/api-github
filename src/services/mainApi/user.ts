6042a4e8b4dea3516051098421fdbebfe0ec8bf0
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 18:14:14 -0300

    inclluindo endpoint get respositories

--- src/services/mainApi/user.ts
+++ src/services/mainApi/user.ts
@@ -1,4 +1,8 @@
 import { LoginUserProps, UserProps } from '../../models/models'
 import baseApi from './config/api'
 
-export const getUser = async (user: LoginUserProps) => baseApi.get<UserProps>(`/users/${user}`)
+export const getUser = async (userLogin: LoginUserProps) =>
+  baseApi.get<UserProps>(`users/${userLogin.userLogin}`)
+
+export const getRepos = async (userLogin: LoginUserProps) =>
+  baseApi.get(`/users/${userLogin.userLogin}/repos`)
