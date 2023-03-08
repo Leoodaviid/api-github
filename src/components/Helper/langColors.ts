813ab7e7108e205284ca3a560c8d616181ccedf4
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 03:43:09 -0300

    Incluindo cores das linguagens

---
+++ src/components/Helper/langColors.ts
@@ -0,0 +1,17 @@
+export const languageColors: { [key: string]: string } = {
+  javascript: '#F1e05a',
+  typescript: '#3178c6',
+  python: '#2ecc71',
+  html: '#e34c26',
+  css: '#563d7c',
+  scss: '#c6538c',
+  less: '#1d365d',
+  vue: '#1abc9c',
+  black: '#0C0D0F',
+  php: '#3498db',
+  go: '#72aed8',
+  ruby: '#e74c3c',
+  c: '#16a085',
+  'c#': '#27ae60',
+  'c++': '#7f8c8d',
+}
