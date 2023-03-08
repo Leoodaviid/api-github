bc2258ff30518546ed68de499651ce1319ce0a8d
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 03:49:42 -0300

    logica de busca p/ linguagens + novo array incluindo colors

---
+++ src/services/langRepositories.ts
@@ -0,0 +1,22 @@
+import { RespositoriesProps } from '../models/models'
+import { languageColors } from '../components/Helper/langColors'
+
+export const getLangsFrom = (repositories: RespositoriesProps[]) => {
+  const languageCounts: { [key: string]: number } = {}
+  repositories.forEach((language) => {
+    if (languageCounts[language.language]) {
+      languageCounts[language.language] += 1
+    } else {
+      languageCounts[language.language] = 1
+    }
+  })
+  const languageData = Object.keys(languageCounts)
+    .map((language) => ({
+      name: language,
+      count: languageCounts[language],
+      color: languageColors[language.toLowerCase()],
+    }))
+    .sort((a, b) => b.count - a.count)
+
+  return languageData
+}
