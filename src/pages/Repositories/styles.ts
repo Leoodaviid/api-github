200d497fc061d0de50120977e2a4ee729a5defb6
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 18:14:24 -0300

    ajustes

--- src/pages/Repositories/styles.ts
+++ src/pages/Repositories/styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
+export const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`
+
 export const Container = styled.main`
   display: flex;
   min-height: 100vh;
@@ -11,15 +18,15 @@ export const Container = styled.main`
 export const Sidebar = styled.aside`
   background: ${({ theme }) => theme.colors.black};
   min-width: 20rem;
-  max-height: 100vh;
-  overflow-y: hidden;
+  min-height: 100vh;
+  /* overflow-y: auto; */
 `
 
 export const Main = styled.section`
   width: 100%;
   max-height: 100vh;
   padding: 2rem;
-  /* overflow-y: hidden; */
+  overflow-y: auto;
 
   @media screen and (max-width: ${({ theme }) => theme.breakPoints.md}) {
     width: auto;
