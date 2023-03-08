bae22c9f3047094bed5b47251f54af8cca63e84b
Leoodaviid <david_ax7@hotmail.com>
2023-03-08 03:41:07 -0300

    incluindo models

---
+++ src/models/models.ts
@@ -0,0 +1,24 @@
+export interface LangColorsProps {
+  javascript: string
+  typescript: string
+  python: string
+  html: string
+  css: string
+  scss: string
+  less: string
+  vue: string
+  black: string
+  php: string
+  go: string
+  ruby: string
+  c: string
+  'c#': string
+  'c++': string
+}
+
+export interface RespositoriesProps {
+  name: string
+  desciption: string
+  html_url: string
+  language: string
+}
