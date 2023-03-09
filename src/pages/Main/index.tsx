6a055ee4eec287ba9aee3f9045a4380063f06051
Leoodaviid <david_ax7@hotmail.com>
2023-03-09 19:26:34 -0300

    retirando link e useparams. usando dispatch

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,22 +1,37 @@
-import { Container, Logo, Title, Form, Input, Button } from './styles'
+import { Container, Logo, Title, Form, Button } from './styles'
+import { FormEvent } from 'react'
 import { MdSearch } from 'react-icons/md'
-import GitHubLogo from '../../assets/icons/github-logo.svg'
+import { FormActions } from '../../context/RepositoriesContext'
+import { Input } from '../../components/Form/Input'
+import { useNavigate } from 'react-router-dom'
 import { useRepo } from '../../hooks/useRepo'
+import GitHubLogo from '../../assets/icons/github-logo.svg'
+import useForm from '../../hooks/useForm'
 
 const Main = () => {
-  const { setLogin, login } = useRepo()
+  const { dispatch } = useRepo()
+  const navigate = useNavigate()
+  const user = useForm('login')
+
+  const handleNext = (event: FormEvent) => {
+    event.preventDefault()
+    if (user.validate()) {
+      dispatch({
+        type: FormActions.setLogin,
+        payload: user.value,
+      })
+      navigate(`/${user.value}/repositories`)
+    }
+  }
+
   return (
     <Container>
       <Logo src={GitHubLogo} alt='Logo Api GitHub' />
       <Title>Api GitHub</Title>
-      <Form>
-        <Input
-          placeholder='Usuário'
-          value={login}
-          onChange={(event) => setLogin(event.target.value)}
-        />
-        <Button to={`/${login}/repositories`}>
-          <MdSearch size={42} />
+      <Form onSubmit={handleNext}>
+        <Input placeholder='Usuário' type='text' label='Usuário' name='login' {...user} />
+        <Button>
+          <MdSearch />
         </Button>
       </Form>
     </Container>
