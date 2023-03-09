2d4b312b39826989b5e08bb5c17b0ab6b67b7d96
Leoodaviid <david_ax7@hotmail.com>
2023-03-09 19:23:37 -0300

    incluindo botão de voltar limpando login atual

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,14 +1,35 @@
-import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md'
+import { MdGroup, MdLocationCity, MdWork, MdLink, MdArrowBack } from 'react-icons/md'
+import { useRepo } from '../../hooks/useRepo'
+import { FormActions } from '../../context/RepositoriesContext'
 import { UserProps } from '../../models/models'
-import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles'
+import { Container, Header, Back, Avatar, Login, Name, Inner, Data } from './styles'
+import { useNavigate } from 'react-router-dom'
 
 interface ProfileProps {
   user?: UserProps
 }
+
 export const Profile = ({ user }: ProfileProps) => {
+  const { dispatch, login } = useRepo()
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    if (login.login) {
+      navigate('/')
+      dispatch({
+        type: FormActions.setLogin,
+        payload: undefined,
+      })
+    }
+  }
+
   return (
     <Container>
       <Header>
+        <Back onClick={handleBack}>
+          <MdArrowBack />
+          Voltar
+        </Back>
         <Avatar src={user?.avatar_url} />
         <Login>{user?.login}</Login>
         <Name>{user?.name}</Name>
