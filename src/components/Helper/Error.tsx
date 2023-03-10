afbe48ee55131e3f937f2ee878b6b61b0981a333
Leoodaviid <david_ax7@hotmail.com>
2023-03-09 21:00:08 -0300

    Styles p/ componente de error

--- src/components/Helper/Error.tsx
+++ src/components/Helper/Error.tsx
@@ -1,10 +1,10 @@
+import { Err } from './styles'
+
 interface ErrorProps {
   children?: string | null
 }
 
 export const Error = ({ children }: ErrorProps) => {
   if (!children) return null
-  return (
-    <p style={{ color: '#f31', position: 'absolute', left: '40%', margin: '1rem 0' }}>{children}</p>
-  )
+  return <Err>{children}</Err>
 }
